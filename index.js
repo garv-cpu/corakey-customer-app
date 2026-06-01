fcd911a5375179f8e0724384553fab963a35b04e
garv-cpu <balwanigarv2006@gmail.com>
2026-06-01 12:44:03 +0530

    news

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 // App entrypoint; registers background FCM handling before React renders.
+import { setJSExceptionHandler, setNativeExceptionHandler } from "react-native-exception-handler";
 import "react-native-gesture-handler";
 import { AppRegistry } from "react-native";
 import messaging from "@react-native-firebase/messaging";
 import App from "./src/App";
 import { handleLockCommand } from "./src/services/fcmService";
+
+setNativeExceptionHandler(errorString => {
+  const RNFS = require("react-native-fs");
+  RNFS.writeFile(
+    `${RNFS.ExternalDirectoryPath}/emi_crash.txt`,
+    `${new Date().toISOString()}\n${errorString}`,
+    "utf8"
+  );
+}, false);
+
+setJSExceptionHandler((error, _isFatal) => {
+  const RNFS = require("react-native-fs");
+  RNFS.writeFile(
+    `${RNFS.ExternalDirectoryPath}/emi_js_crash.txt`,
+    `${new Date().toISOString()}\n${error.message}\n${error.stack}`,
+    "utf8"
+  );
+}, true);
 
 const appName = "CoraCustomerApp";
 
